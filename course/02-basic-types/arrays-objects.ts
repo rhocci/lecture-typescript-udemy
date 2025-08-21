2143aa7e06d3ee27fab01191dc32e146a2b3f391
rhocci <4tres1@gmail.com>
2025-08-21 23:19:31 +0900

    learn: 02 - Glimpse At Generic Types(Array<T>)

--- course/02-basic-types/arrays-objects.ts
+++ course/02-basic-types/arrays-objects.ts
@@ -2,7 +2,8 @@ let hobbies = ['Sports', 'Cooking'];
 
 // hobbies.push(10);
 
-let users: (string | number)[];
+// let users: (string | number)[];
+let users: Array<string | number>;
 
 users = [1, 'Max'];
 users = [1, 2];
