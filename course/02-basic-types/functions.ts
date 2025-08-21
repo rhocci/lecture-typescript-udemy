c4f17492ad859c7c01451c330009b0089cfce7a5
rhocci <4tres1@gmail.com>
2025-08-22 03:07:31 +0900

    learn: 02 - Function Types

--- course/02-basic-types/functions.ts
+++ course/02-basic-types/functions.ts
@@ -9,3 +9,25 @@ function log(message: string): void {
 function logAndThrow(errorMessage: string): never {
   throw new Error(errorMessage);
 }
+
+function performJob(cb: (msg: string) => void) {
+  cb('Job done!');
+}
+
+performJob(log);
+
+type UserInfo = {
+  name: string;
+  age: number;
+  greet: () => string;
+};
+
+let user: UserInfo = {
+  name: 'Max',
+  age: 38,
+  greet() {
+    return `Hello, ${this.name}!`;
+  },
+};
+
+console.log(user.greet());
