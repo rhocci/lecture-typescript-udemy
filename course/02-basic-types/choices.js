99f150b6df6a9845db9274469ea399747e72b44d
rhocci <4tres1@gmail.com>
2025-08-22 00:53:55 +0900

    learn: 02 - Enum Types

---
+++ course/02-basic-types/choices.js
@@ -0,0 +1,9 @@
+var Role;
+(function (Role) {
+    Role[Role["Admin"] = 0] = "Admin";
+    Role[Role["Editor"] = 1] = "Editor";
+    Role[Role["Guest"] = 2] = "Guest";
+})(Role || (Role = {}));
+var userRole = Role.Admin;
+// ...
+userRole = Role.Guest;
---
+++ course/02-basic-types/choices.ts
@@ -0,0 +1,11 @@
+enum Role {
+  Admin, // 0
+  Editor, // 1
+  Guest, // 2
+}
+
+let userRole: Role = Role.Admin;
+
+// ...
+
+userRole = Role.Guest;
