afeb8c84bbc7e4ff8211d2ad87258313d0164cb5
rhocci <4tres1@gmail.com>
2025-08-27 18:26:11 +0900

    solution: 04 - Adding Application Logic

--- course/04-essentials-demo-project/src/calculator.ts
+++ course/04-essentials-demo-project/src/calculator.ts
@@ -14,7 +14,43 @@ type InvestmentResult = {
 
 type CalculationResult = InvestmentResult[] | string;
 
-function calculateInvestment(data: InvestmentData): CalculationResult {}
+function calculateInvestment(data: InvestmentData): CalculationResult {
+  const { initialAmount, annualContribution, expectedReturn, duration } = data;
+
+  if (initialAmount < 0) {
+    return 'Initial investment amount must be at least zero.';
+  }
+
+  if (duration <= 0) {
+    return 'No valid amount of years provided.';
+  }
+
+  if (expectedReturn < 0) {
+    return 'Expected return must be at least zero.';
+  }
+
+  let total = initialAmount;
+  let totalContributions = 0;
+  let totalInterestEarned = 0;
+
+  const annualResults: InvestmentResult[] = [];
+
+  for (let i = 0; i < duration; i++) {
+    total = total * (1 + expectedReturn);
+    totalInterestEarned = total - totalContributions - initialAmount;
+    totalContributions = totalContributions + annualContribution;
+    total = total + annualContribution;
+
+    annualResults.push({
+      year: `Year ${i + 1}`,
+      totalAmount: total,
+      totalContributions,
+      totalInterestEarned,
+    });
+  }
+
+  return annualResults;
+}
 
 function printResults(results) {}
 
