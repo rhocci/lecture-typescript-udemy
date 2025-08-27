902b3722e1a81f05c2fb2646a0907aef1a49f6b4
rhocci <4tres1@gmail.com>
2025-08-27 15:33:04 +0900

    learn: 03 - Compiling with tsconfig & Watch mode

--- course/03-typescript-compiler/dist/app.js
+++ course/03-typescript-compiler/dist/app.js
@@ -3,4 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 let userName;
 userName = 'Max';
 console.log(userName);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
 //# sourceMappingURL=app.js.map
--- course/03-typescript-compiler/src/app.ts
+++ course/03-typescript-compiler/src/app.ts
@@ -3,3 +3,9 @@ let userName: string;
 userName = 'Max';
 
 console.log(userName);
+
+function add(a: any, b: any) {
+  return a + b;
+}
+
+console.log(add(1, 2));
