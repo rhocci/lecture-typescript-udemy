eccdc14bae43635d013a7e6a33ff45b4a597c37e
rhocci <4tres1@gmail.com>
2025-08-27 17:46:17 +0900

    exercise: 04 - Create calculateInvestment Function

--- course/04-essentials-demo-project/calculator.ts
+++ course/04-essentials-demo-project/calculator.ts
@@ -1,14 +1,51 @@
+// 타입 정의
+type dataType = {
+  initialInvestment: number;
+  annualInvestment: number;
+  expectedReturn: number;
+  duration: number;
+};
+
+type resultType = {
+  year: number;
+  total: number;
+  totalInvestment: number;
+  totalReturn: number;
+};
+
 // data: { string: number }
 // initial amount(초기 투자액)
 // annual contribution(연간 추가 투자액)
 // expected return(기대 수익률)
 // duration(투자 기간)
-function calculateInvestment(data) { } // = > result[]
+function calculateInvestment(data: dataType): resultType[] {
+  const results: resultType[] = [];
+  const { initialInvestment, annualInvestment, expectedReturn, duration } =
+    data;
 
-function printResults(results) {
-  // print (output) the result data
-}
+  for (let i = 0; i < duration; i++) {
+    const currentInvestment = initialInvestment + annualInvestment * i;
+    const currentTotal =
+      currentInvestment + (currentInvestment * expectedReturn) / 100;
+    const currentReturn = currentTotal - currentInvestment;
 
-const results = calculateInvestment(...);
+    const result: resultType = {
+      year: i,
+      total: currentTotal,
+      totalInvestment: currentInvestment,
+      totalReturn: currentReturn,
+    };
 
-printResults(results);
+    results.push(result);
+  }
+
+  return results;
+} // = > result[]
+
+// function printResults(results) {
+//   // print (output) the result data
+// }
+
+// const results = calculateInvestment();
+
+// printResults(results);
