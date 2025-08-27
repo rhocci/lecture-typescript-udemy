6d91b0ec5394cdb433227e2ccc4602b054ac7a76
rhocci <4tres1@gmail.com>
2025-08-27 17:00:41 +0900

    learn: 04 - Demo Project Setup

---
+++ course/04-essentials-demo-project/calculator.ts
@@ -0,0 +1,14 @@
+// data: { string: number }
+// initial amount(초기 투자액)
+// annual contribution(연간 추가 투자액)
+// expected return(기대 수익률)
+// duration(투자 기간)
+function calculateInvestment(data) { } // = > result[]
+
+function printResults(results) {
+  // print (output) the result data
+}
+
+const results = calculateInvestment(...);
+
+printResults(results);
