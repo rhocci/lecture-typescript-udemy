cd5f071c1e926b238e87ec25414287ebd4ce036e
rhocci <4tres1@gmail.com>
2025-09-10 21:00:09 +0900

    learn: 08 - Generic Classes & Interfaces

--- course/08-generic-types/generics.ts
+++ course/08-generic-types/generics.ts
@@ -22,3 +22,15 @@ function mergeObj<T extends object, U extends object>(a: T, b: U) {
 
 const merged = mergeObj({ userName: 'Max' }, { age: 35 });
 console.log(merged);
+
+interface Role<T> {
+  admin: T;
+  user: T;
+}
+
+class User<T> {
+  constructor(public id: T) {}
+}
+
+const user = new User('i1');
+user.id;
