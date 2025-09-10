efb6c23a5a9e5a1d88dc78fb8aeb18268f4396c1
rhocci <4tres1@gmail.com>
2025-09-10 22:36:24 +0900

    learn: 09 - Adding Items More Efficiently

--- course/09-classes-generics-practice/linked-list.ts
+++ course/09-classes-generics-practice/linked-list.ts
@@ -6,18 +6,17 @@ class ListNode<T> {
 
 class LinkedList<T> {
   private root?: ListNode<T>;
+  private tail?: ListNode<T>;
   private length = 0;
 
   add(value: T) {
     const node = new ListNode(value);
-    if (!this.root) {
+    if (!this.root || !this.tail) {
       this.root = node;
+      this.tail = node;
     } else {
-      let current = this.root;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = node;
+      this.tail.next = node;
+      this.tail = node;
     }
     this.length++;
   }
