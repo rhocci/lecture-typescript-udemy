08a703f8cccc9ee83cfcb8a8d3dab1224184173a
rhocci <4tres1@gmail.com>
2025-09-10 17:28:00 +0900

    learn: 07 - Constant Types with "as const"

--- course/07-advanced-types/advanced.ts
+++ course/07-advanced-types/advanced.ts
@@ -9,3 +9,7 @@ let store: DataStore = {};
 store.id = 5;
 store.isOpen = false;
 // store.name = 'Max';
+
+let roles = ['admin', 'guest', 'editor'] as const;
+// roles.push('max');
+const firstRole = roles[0];
