d06d562c2434c2a7bed883f3263d0cba32ba5082
rhocci <4tres1@gmail.com>
2025-09-10 16:57:08 +0900

    learn: 07 - Function Overloads

---
+++ course/07-advanced-types/functions.ts
@@ -0,0 +1,12 @@
+function getLength(val: string | any[]) {
+  if (typeof val === 'string') {
+    const numberOfWords = val.split(' ').length;
+    return `${numberOfWords} words`;
+  }
+
+  return val.length;
+}
+
+const numOfWords = getLength('does this work?');
+// numOfWords.length;
+const numItems = getLength(['Sports', 'Cookies']);
