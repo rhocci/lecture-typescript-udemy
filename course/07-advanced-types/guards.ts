fe28cb8437ab5ddf7793e6104beda8d947ff332c
rhocci <4tres1@gmail.com>
2025-09-10 15:57:13 +0900

    learn: 07 - Type Guards via "instanceof"

--- course/07-advanced-types/guards.ts
+++ course/07-advanced-types/guards.ts
@@ -1,29 +1,54 @@
-type FileSource = {
-  type: 'file';
-  path: string;
-};
-type DBSource = {
-  type: 'db';
-  connectionUrl: string;
-};
-
-type Source = FileSource | DBSource;
-
+type FileSource = { type: 'file'; path: string };
 const fileSource: FileSource = {
   type: 'file',
   path: 'some/path/to/file.csv',
 };
+
+type DBSource = { type: 'db'; connectionUrl: string };
 const dbSource: DBSource = {
   type: 'db',
   connectionUrl: 'some-connection-url',
 };
 
+type Source = FileSource | DBSource;
+
 function loadData(source: Source) {
-  // fileSource일 경우 path 열기
+  // if ('path' in source) {
   if (source.type === 'file') {
-    source.path;
+    // source.path
+    // source.path; => use that to open the file
     return;
   }
-  // dbSource일 경우 connectionUrl 열기
-  source.connectionUrl;
+  // source.connectionUrl; => to reach out to database
+}
+
+class User {
+  constructor(public name: string) {}
+
+  join() {
+    // ...
+  }
+}
+
+class Admin {
+  constructor(permissions: string[]) {}
+
+  scan() {
+    // ...
+  }
+}
+
+const user = new User('Max');
+const admin = new Admin(['ban', 'restore']);
+
+type Entity = User | Admin;
+
+function init(entity: Entity) {
+  // .join() OR .scan() ...
+  if (entity instanceof User) {
+    entity.join();
+    return;
+  }
+
+  entity.scan();
 }
