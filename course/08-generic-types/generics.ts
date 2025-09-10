c3530d99d31928a1cbb259430c0ee7d1ba9d859a
rhocci <4tres1@gmail.com>
2025-09-10 20:19:47 +0900

    learn: 08 - Creating & Using a Generic Type

--- course/08-generic-types/generics.ts
+++ course/08-generic-types/generics.ts
@@ -1,1 +1,11 @@
 let names: Array<string> = ['Max', 'Anna'];
+
+type DataStore<T> = {
+  [key: string]: T;
+};
+
+let store: DataStore<string | boolean> = {};
+store.name = 'Max';
+store.isInstructor = true;
+
+let nameStore: DataStore<string> = {};
