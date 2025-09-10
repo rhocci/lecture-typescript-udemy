5c72f6d06dbbfa8299e2fbf18e8114381fedffb8
rhocci <4tres1@gmail.com>
2025-09-10 12:10:33 +0900

    learn: 07 - Discriminated Unions

--- course/07-advanced-types/guards.ts
+++ course/07-advanced-types/guards.ts
@@ -1,18 +1,26 @@
-type FileSource = { path: string };
-type DBSource = { connectionUrl: string };
+type FileSource = {
+  type: 'file';
+  path: string;
+};
+type DBSource = {
+  type: 'db';
+  connectionUrl: string;
+};
 
 type Source = FileSource | DBSource;
 
 const fileSource: FileSource = {
+  type: 'file',
   path: 'some/path/to/file.csv',
 };
 const dbSource: DBSource = {
+  type: 'db',
   connectionUrl: 'some-connection-url',
 };
 
 function loadData(source: Source) {
   // fileSource일 경우 path 열기
-  if ('path' in source) {
+  if (source.type === 'file') {
     source.path;
     return;
   }
